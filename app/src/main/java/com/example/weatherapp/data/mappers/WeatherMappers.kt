80554a48352a3494d64c0399776eb2d13a8c53c3
mehdi <dehghani_m@rocketmail.com>
2022-10-05 20:34:15 +0330

    try make more models

--- app/src/main/java/com/example/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/example/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,2 +1,36 @@
 package com.example.weatherapp.data.mappers
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.weatherapp.data.remote.WeatherDataDto
+import com.example.weatherapp.domain.weather.WeatherData
+import com.example.weatherapp.domain.weather.WeatherType
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+data class IndexedWeatherData(
+    val index : Int,
+    val data : WeatherData
+)
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun WeatherDataDto.toWeatherDataMap(): Map<Int , List<WeatherData>>{
+      return time.mapIndexed{ index, time ->
+          val temperature = temperatures[index]
+          val weatherCode = weatherCodes[index]
+          val windSpeed = windSpeeds[index]
+          val pressure = pressures[index]
+          val humidity = humidities[index]
+          WeatherData(
+              time = LocalDateTime.parse(time , DateTimeFormatter.ISO_DATE_TIME),
+              temperatureCelsius = temperature,
+              pressure = pressure,
+              windSpeed = windSpeed,
+              humidity = humidity,
+              weatherType = WeatherType.fromWMO(weatherCode)
+          )
+      }.groupBy {
+          it.time.dayOfMonth
+      }
+}
