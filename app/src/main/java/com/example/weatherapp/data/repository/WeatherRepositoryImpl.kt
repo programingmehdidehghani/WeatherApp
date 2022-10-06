40eda912d6f5d764cb36931d04ad7be57f70d738
mehdi <dehghani_m@rocketmail.com>
2022-10-06 14:36:20 +0330

    add repository in data

--- app/src/main/java/com/example/weatherapp/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/WeatherRepositoryImpl.kt
@@ -1,5 +1,8 @@
 package com.example.weatherapp.data.repository
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.weatherapp.data.mappers.toWeatherInfo
 import com.example.weatherapp.data.remote.WeatherApi
 import com.example.weatherapp.domain.repository.WeatherRepository
 import com.example.weatherapp.domain.util.Resource
@@ -8,9 +11,20 @@ import javax.inject.Inject
 
 class WeatherRepositoryImpl @Inject constructor(
     private val api: WeatherApi
-) : WeatherRepository{
+) : WeatherRepository {
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
-        TODO("Not yet implemented")
+        return try {
+            Resource.Success(
+                data = api.getWeatherData(
+                    lat = lat ,
+                    long = long
+                ).toWeatherInfo()
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.Error(e.message ?: "An unknown error occurred.")
+        }
     }
 
 
