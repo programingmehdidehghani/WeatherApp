d2f5f49e03a9abb066498bcb9bd1d2210dcc20e1
mehdi <dehghani_m@rocketmail.com>
2022-10-06 14:54:49 +0330

    try make check permissions

--- app/src/main/java/com/example/weatherapp/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/com/example/weatherapp/data/location/DefaultLocationTracker.kt
@@ -1,4 +1,35 @@
 package com.example.weatherapp.data.location
 
-class DefaultLocationTracker {
+import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
+import com.example.weatherapp.domain.location.LocationTracker
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.util.jar.Manifest
+import javax.inject.Inject
+
+class DefaultLocationTracker @Inject constructor(
+    private val locationClient: FusedLocationProviderClient,
+    private val application: Application
+) : LocationTracker {
+
+    override suspend fun getCurrentLocation(): Location? {
+        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
 }
