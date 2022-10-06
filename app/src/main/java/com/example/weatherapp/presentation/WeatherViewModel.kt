52b4d9469cebee42c24bd05ca3b73519bb82f072
mehdi <dehghani_m@rocketmail.com>
2022-10-06 23:28:42 +0330

    start make view model

---
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.weatherapp.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.domain.location.LocationTracker
+import com.example.weatherapp.domain.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val repository: WeatherRepository,
+    private val locationTracker: LocationTracker
+) : ViewModel(){
+
+    var state by mutableStateOf(WeatherState())
+     private set
+
+    fun loadWeatherInfo(){
+        viewModelScope.launch {
+
+        }
+    }
+}
