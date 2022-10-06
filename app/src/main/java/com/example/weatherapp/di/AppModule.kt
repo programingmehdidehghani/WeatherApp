b552a4472756fc981c196fe8373f5f3377627222
mehdi <dehghani_m@rocketmail.com>
2022-10-06 15:14:40 +0330

    try make di retrofit

--- app/src/main/java/com/example/weatherapp/di/AppModule.kt
+++ app/src/main/java/com/example/weatherapp/di/AppModule.kt
@@ -1,11 +1,30 @@
 package com.example.weatherapp.di
 
+import com.example.weatherapp.data.remote.WeatherApi
+import com.squareup.moshi.Moshi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi() : WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.open-meteo.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+
 }
