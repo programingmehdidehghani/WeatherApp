d26ddd973b5738e28f73cb15e2d3f642e0e8ed93
mehdi <dehghani_m@rocketmail.com>
2022-10-07 11:17:04 +0330

    finish display

--- app/src/main/java/com/example/weatherapp/presentation/WeatherDataDisplay.kt
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherDataDisplay.kt
@@ -1,13 +1,19 @@
 package com.example.weatherapp.presentation
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WeatherDataDisplay(
@@ -22,7 +28,17 @@ fun WeatherDataDisplay(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(bitmap = , contentDescription = )
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = iconTint,
+            modifier = Modifier.size(25.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "$value$unit",
+            style = textStyle
+        )
     }
 
 }
