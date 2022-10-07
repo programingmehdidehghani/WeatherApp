6450b535cd6de2399309e5bbf5664774cf0f7505
mehdi <dehghani_m@rocketmail.com>
2022-10-07 11:12:05 +0330

    try1 make compose weather display

--- app/src/main/java/com/example/weatherapp/presentation/WeatherDataDisplay.kt
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherDataDisplay.kt
@@ -1,8 +1,28 @@
 package com.example.weatherapp.presentation
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 
 @Composable
-fun WeatherDataDisplay(){
-    
+fun WeatherDataDisplay(
+    value : Int,
+    unit : String,
+    icon : ImageVector,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = TextStyle(),
+    iconTint : Color = Color.White
+){
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(bitmap = , contentDescription = )
+    }
+
 }
