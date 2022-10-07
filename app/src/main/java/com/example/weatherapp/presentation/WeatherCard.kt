eee854ffbf0b0e4d9ce6f4f9184e03c02c13b62d
mehdi <dehghani_m@rocketmail.com>
2022-10-07 10:55:24 +0330

    continue make compose

--- app/src/main/java/com/example/weatherapp/presentation/WeatherCard.kt
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherCard.kt
@@ -1,34 +1,50 @@
 package com.example.weatherapp.presentation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun WeatherCard(
     state: WeatherState,
     backgroundColor: Color,
     modifier: Modifier = Modifier
 ) {
-    Card(
-        backgroundColor = backgroundColor,
-        shape = RoundedCornerShape(10.dp),
-        modifier = modifier.padding(16.dp)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-                .padding(16.dp)
+    state.weatherInfo?.currentWeatherData?.let { data ->
+        Card(
+            backgroundColor = backgroundColor,
+            shape = RoundedCornerShape(10.dp),
+            modifier = modifier.padding(16.dp)
         ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Today ${
+                        data.time.format(
+                            DateTimeFormatter.ofPattern("HH:mm")
+                        )
+                    }",
+                    modifier = Modifier.align(Alignment.End),
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
 
         }
-
     }
+
 
 }
