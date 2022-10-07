da2586842bf4df7fbd58bc3cf69b880f1537ff92
mehdi <dehghani_m@rocketmail.com>
2022-10-07 10:48:11 +0330

    make ui weather card

--- app/src/main/java/com/example/weatherapp/presentation/WeatherCard.kt
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherCard.kt
@@ -1,2 +1,34 @@
 package com.example.weatherapp.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        backgroundColor = backgroundColor,
+        shape = RoundedCornerShape(10.dp),
+        modifier = modifier.padding(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+        }
+
+    }
+
+}
